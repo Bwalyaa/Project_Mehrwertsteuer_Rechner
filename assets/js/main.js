f7a6599ab5415d4ab93d7cc48b7d231d083647f4
Bwalyaa <bwalya@hotmail.de>
2023-12-15 13:45:49 +0100

    first push

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+const goCalculate = () => {
+    const inputText = +document.querySelector("#netMoney").value
+    const aufschlagen = document.querySelector("#aufschlag").checked
+    const abziehen = document.querySelector("#abziehen").checked
+    
+    const nineteenPro = document.querySelector("#nineteenPercent").checked
+    const sevenPro =document.querySelector("#sevenPercent").checked
+
+    
+}
+
