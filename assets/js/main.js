66ae6cfb32f49e158ba12948b33bb09858ce065e
Bwalyaa <bwalya@hotmail.de>
2023-12-17 22:10:14 +0100

    finito

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,43 +1,42 @@
-const goCalculate = () => {
-    const inputText = +document.querySelector("#netMoney").value
-    const aufschlagen = document.querySelector("#aufschlag").checked
-    const abziehen = document.querySelector("#abziehen").checked
-    
-    const nineteenPro = document.querySelector("#nineteenPercent").checked
-    const sevenPro = document.querySelector("#sevenPercent").checked
 
-    const resultTax = document.querySelector(".resultTaxCosts")
-    const resultBrutto = document.querySelector(".resultBruttoCosts")
+const goCalculate = () => {
+    const inputText = +document.querySelector("#netMoney").value;
+    const aufschlagen = document.querySelector("#aufschlag").checked;
+    const abziehen = document.querySelector("#abziehen").checked;
+
+    const nineteenPro = document.querySelector("#nineteenPercent").checked;
+    const sevenPro = document.querySelector("#sevenPercent").checked;
+
+    const resultTax = document.querySelector(".resultTaxCosts");
+    const resultBrutto = document.querySelector(".resultBruttoCosts");
+
+    const nineteen = inputText * 0.19;
+    const seven = inputText * 0.07;
 
     // Mehrertsteuer drauf
-    const calAmountNine = (inputText * 0.19).toFixed(2)
-    const calAmountSeven = (inputText * 0.07).toFixed(2) // kann scheinbar nicht 0,07 schreiben
-    const calBruttoBetragPlus = (inputText + calAmountNine);
-    const calBruttoBetragPlus2 = (inputText + calAmountSeven)
+    const calAmountNine = inputText * (1 + 0.19);
+    const calAmountSeven = inputText * (1 + 0.07);
 
-    // Mehrwersteuer abziehe
-    // const calAmountNinePlus = (inputText * 0,19); // 
-    // const calculateAmountSevenPlus = (inputText * 0,07); // diese beiden codes bleiben dieselben
-    const calNettoBetragMinus = (inputText - calAmountNine)
-    const calNettoBetragMinus2 = (inputText - calAmountSeven)
+    // Mehrwertsteuer abziehen
+    const calNettoBetragMinus = inputText / (1 + 0.19);
+    const calNettoBetragMinus2 = inputText / (1 + 0.07);
 
     if (aufschlagen && nineteenPro) {
-        resultTax.innerHTML = calAmountNine
-        resultBrutto.innerHTML = calBruttoBetragPlus
-
-    } else if (aufschlagen && sevenPro)
-        resultTax.innerHTML = calAmountSeven
-        resultBrutto.innerHTML = calBruttoBetragPlus2
+        resultTax.innerHTML = nineteen.toFixed(2);
+        resultBrutto.innerHTML = calAmountNine.toFixed(2);
+    } else if (aufschlagen && sevenPro) {
+        resultTax.innerHTML = seven.toFixed(2);
+        resultBrutto.innerHTML = calAmountSeven.toFixed(2);
+    }
 
     if (abziehen && nineteenPro) {
-        resultTax.innerHTML = calAmountNine
-        resultBrutto.innerHTML = calNettoBetragMinus
-
-    } else if (abziehen && sevenPro)
-        resultTax.innerHTML = calAmountSeven
-        resultBrutto.innerHTML = calNettoBetragMinus2
+        resultTax.innerHTML = nineteen.toFixed(2);
+        resultBrutto.innerHTML = calNettoBetragMinus.toFixed(2);
+    } else if (abziehen && sevenPro) {
+        resultTax.innerHTML = seven.toFixed(2);
+        resultBrutto.innerHTML = calNettoBetragMinus2.toFixed(2);
+    }
 }
-
 
 
 
